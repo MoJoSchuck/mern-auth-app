fed18ff9000ddb0ed2ed85686c5019b6c8cd37a4
Moritz Schuck <159003684+MoJoSchuck@users.noreply.github.com>
2025-08-06 10:37:52 +0200

    refactor: Switch to index.js as main entry point with nodemon setup

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,11 @@ import express from 'express';
 
 const app = express();
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 })
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+})
+
